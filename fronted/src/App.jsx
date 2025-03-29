3cd988e7a05ada88147bbcb2ecc7b136d66fea2c
nishan-khiva <nishankhiva@gmail.com>
2025-03-29 21:14:01 +0530

    first commit

---
+++ fronted/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import Joker from './components/joker'
+
+function App() {
+  
+  return (
+    <>
+    <Joker/>
+    <Joker name="nishan Singh"/>
+    </>
+  )
+}
+
+export default App
---
+++ fronted/src/components/joker.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from "react";
+
+
+function Joker({name}) {
+    let [joke, setJoke] = useState({ } );
+    const URL = "https://official-joke-api.appspot.com/random_joke";
+
+    const getNewjoke = async () => {
+        let response = await fetch(URL);
+        let jsonResponse = await response.json();
+        console.log(jsonResponse);
+        setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
+    }
+    useEffect(()=>{
+        async function getFirstJoke(){
+            let response = await fetch(URL);
+            let jsonResponse = await response.json();
+            console.log(jsonResponse);
+            setJoke({ setup: jsonResponse.setup, punchline: jsonResponse.punchline });
+
+        }
+        getFirstJoke();
+    } ,[])
+
+
+    return (
+        <>
+            <h2>This is {name} page</h2>
+            <h3>{joke.setup}</h3>
+            <h3>{joke.punchline}</h3>
+            <button onClick={getNewjoke}>click me</button>
+
+        </>
+    )
+}
+
+export default Joker
+
+
